c526e91d7616ad55166c48c7faeb03e0a80c7f13
Matías <matiasnietoperez@gmail.com>
2026-03-21 22:39:36 -0500

    feat: guardado de seguridad

---
+++ src/config.py
@@ -0,0 +1,8 @@
+#las variables tienen el nombre de unidad de medida del S.I o del sistema Anglosajon, sus valores son la cantidad a la que equivalen en la unidad de medida usada para lo mismo en el sistema opuesto
+kilogramo=2
+libra=0.5
+kilometro=1.6
+milla=0.6
+hectarea=2.47
+acre=0.404
+equivalencias={"kilogramo": libra, "libra": kilogramo, "kilometro": milla, "milla": kilometro, "hectarea": acre, "acre": hectarea}
