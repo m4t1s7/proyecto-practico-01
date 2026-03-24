4b1d58269ae9ec80be5df29a01468507719f902f
Matías <matiasnietoperez@gmail.com>
2026-03-23 19:04:53 -0500

    feat: función limpiar:manejo de tildes y mayusculas

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from src.config import equivalencias, comparadas
-from src.utils import convertir, comparar
+from src.utils import convertir, comparar, limpiar
 
-unidad_a_convertir=input("Qué unidad quieres convertir?(escribela en minuscula)(elige entre kilogramo, libra, kilometro, milla, acre y hectarea): ") #Solicitamos la unidad de medida que buscaremos convertir
+unidad_a_convertir=limpiar(input("Qué unidad quieres convertir?(escribela en minuscula)(elige entre kilogramo, libra, kilometro, milla, acre y hectarea): ")) #Solicitamos la unidad de medida que buscaremos convertir
 valor_unidad_a_convertir=float(input("ahora introduce el valor de esa unidad: "))#Acá sabemos la cantidad de unidades
 
-print(f"listo, tu conversión resulta en que {valor_unidad_a_convertir} {unidad_a_convertir} equivalen a", convertir(unidad_a_convertir, valor_unidad_a_convertir, equivalencias), comparar(unidad_a_convertir, valor_unidad_a_convertir,comparadas))
+print(f"listo, tu conversión resulta en que {valor_unidad_a_convertir} {unidad_a_convertir} equivalen a", convertir(unidad_a_convertir, valor_unidad_a_convertir, equivalencias), comparar(unidad_a_convertir,comparadas))
--- src/utils.py
+++ src/utils.py
@@ -4,3 +4,9 @@ def convertir(unidad: str, valor: float, equivalencias: dict) -> float:
 #Ésta función referenciará la medida a la cual convertimos
 def comparar(unidad_a_convertir: str,comparadas: dict) -> str:
     return comparadas[unidad_a_convertir]
+def limpiar(texto: str) -> str:
+    reemplazos = {"á":"a", "é":"e", "í":"i", "ó":"o", "ú":"u"}
+    texto = texto.lower()
+    for tilde, sin_tilde in reemplazos.items():
+        texto = texto.replace(tilde, sin_tilde)
+    return texto
