a1c14e3fb37ba3e698b61541b7aedaafe59bba6d
Matías <matiasnietoperez@gmail.com>
2026-03-27 22:47:32 -0500

    feat: manejo de errores en consulta de clima

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from src.config import API_KEY
 from src.clima import get_clima
 
-datos = get_clima("Madrid", API_KEY)
-temp=datos["main"]['temp']
-descripción=datos["weather"][0]["description"]
-ciudad=datos["name"]
-print(f"en {ciudad} hay una temperatura de {temp}C° y un éstado general: {descripción} ")
+try:
+    datos = get_clima("jaisdnj", API_KEY)
+    temp=datos["main"]['temp']
+    descripción=datos["weather"][0]["description"]
+    ciudad=datos["name"]
+    print(f"en {ciudad} hay una temperatura de {temp}C° y un éstado general: {descripción} ")
+except Exception as e:
+    print(f"Error al consultar el clima: {e}")
